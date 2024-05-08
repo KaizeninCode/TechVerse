d43eb998d348bdb0ed2372baec4b3293222c58a9
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-08 23:30:43 +0300

    Created the category model

--- server/models/categories.py
+++
@@ -1,4 +0,0 @@
-from .dbconfig import db
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
