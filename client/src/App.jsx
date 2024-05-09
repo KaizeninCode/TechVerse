f68a4df91138ebd1499dbd1f631bcaa1086eabc8
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-09 09:38:16 +0300

    ft:app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import {Routes,Route } from 'react-router-dom'
 import Post from './pages/Post'
+import Profile from './pages/profile/Profile'
 const App = () => {
 
   return (
     <>
+      <Post/>
       <Routes>
-        <Route path='/post' element={<Post/>}/>
+        <Route path='/profile' element={<Profile/>} />
       </Routes>
     </>
   )
