a4dedbac000287c50f9ad00daf36358e2c787206
Allen <allenshamrock37@gmail.com>
2024-05-09 10:28:01 +0300

    {Fet}:Implemented form validation using formik & yup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import UserAuth from './components/UserAuth'
 const App = () => {
 
   return (
@@ -6,6 +6,7 @@ const App = () => {
       <h1 className="text-3xl font-montserrat font-bold text-center mt-12">
         TECHVERSE
       </h1>
+      <UserAuth/>
     </>
   )
 }
