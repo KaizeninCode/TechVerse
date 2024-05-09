c753385b8b12364a2f395fda288fcf04419a5094
Mariam Senzia <mariamsenzia@gmail.com>
2024-05-09 10:54:33 +0300

    resolved conflicts

--- server/app.py
+++ server/app.py
@@ -10,7 +10,6 @@ from models.content import Content
 from models.subscription import Subscription
 from models.user import User
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
