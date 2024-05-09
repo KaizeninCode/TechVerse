651bd6d4f70a625caa269f7338ce8cc99c22234d
Allen <allenshamrock37@gmail.com>
2024-05-09 10:28:01 +0300

    {Fet}:Implemented form validation using formik & yup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from "@chakra-ui/react";
-<<<<<<< HEAD
 import {BrowserRouter as Router} from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react''
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -18,14 +17,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ChakraProvider>
   </React.StrictMode>
 );
-=======
-import { ChakraProvider } from "@chakra-ui/react";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
->>>>>>> a4dedba ({Fet}:Implemented form validation using formik & yup)
