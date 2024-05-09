33cbc726a3fc79744777e89ff9a60da1941925f8
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-09 09:08:30 +0300

    Resolved the main branch

--- server/models/subscription.py
+++
@@ -1,9 +0,0 @@
-from .dbconfig import db
-
-class Subscription(db.Model):
-    __tablename__ = 'subscriptions'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    
