3bbc7763a56748498bbb7797e429cb0f110f646b
Mariam Senzia <mariamsenzia@gmail.com>
2024-05-10 09:38:57 +0300

    resolved conflicts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from "@chakra-ui/react";
+<<<<<<< HEAD
 import {BrowserRouter as Router} from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -17,3 +18,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ChakraProvider>
   </React.StrictMode>
 );
+=======
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
+>>>>>>> a4dedba ({Fet}:Implemented form validation using formik & yup)
