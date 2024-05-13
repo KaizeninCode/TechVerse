0838763fb839095b58d9d69ef3e63a1eb10c39ab
Mariam Senzia <mariamsenzia@gmail.com>
2024-05-13 20:02:24 +0300

    modified app.py

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+<<<<<<< HEAD
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router } from'react-router-dom'
 import ThemeProvider from './components/ThemeContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
+=======
+import { ChakraProvider } from "@chakra-ui/react";
+import {BrowserRouter as Router} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react''
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+>>>>>>> 651bd6d ({Fet}:Implemented form validation using formik & yup)
   <React.StrictMode>
  <ThemeProvider>
    <ChakraProvider>
