ccd8b303192981e2b64d59e93954e4e10e64dd54
Ian <iankosgey535@gmail.com>
2024-05-21 07:37:33 +0300

    Added thumb up and down icons.

--- client/src/components/PostContainer.jsx
+++ client/src/components/PostContainer.jsx
@@ -21,95 +21,55 @@ import { RiShareForwardLine } from "react-icons/ri";
 import colorPallete from './colorPallete';
 import { AiOutlineLike, AiOutlineDislike } from 'react-icons/ai';
 const PostContainer = () => {
-  const theme = colorPallete();
-  const [content, setContent] = useState([]);
-  const { colorMode } = useColorMode();
-    const [input, setInput] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const filterPosts = content.filter((post) => {
-    const searchPost =
-      input === '' ||
-      post.title.toUpperCase().startsWith(input.toUpperCase());
-    const SetCategory =
-      selectedCategory === null || post.category === selectedCategory;
-    return searchPost && SetCategory;
-  });
-  const { isOpen, handleDisclose } = useDisclosure();
+    const theme = colorPallete();
+    const [content, setContent] = useState([]);
+    const { colorMode } = useColorMode();
 
-  useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        const response = await fetch("http://localhost:5555/contents");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setContent(data);
-      } catch (error) {
-        console.error(
-          "There has been a problem with your fetch operation:",
-          error
-        );
-      }
-    };
+    useEffect(() => {
+        const fetchContent = async () => {
+            try {
+                const response = await fetch('http://localhost:5555/contents');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setContent(data);
+            } catch (error) {
+                console.error('There has been a problem with your fetch operation:', error);
+            }
+        };
 
-    fetchContent();
-  }, []);
-  function HandleChange(e) {
-      setInput(e.target.value);
-    }
-  return (
-    <SimpleGrid
-      className="lg:w-[80%] overflow-y-scroll gap-4 p-4 mx-5 my-3 border border-gray-400 rounded-md"
-      id="posts">
-      <SearchBar  handleChange={HandleChange} value={input}/>
-    
-      {filterPosts?.map(post => (
-          <Card key={post.id} bg={theme.bg} color={theme.color} className='border-b border-gray-400'>
-            <CardHeader className='flex justify-between'>
-           <Link to={`/posts/${post.id}`} state={{post}}>
-            <Flex justify={'space-between'} alignItems={'center'}>
-                <Image src={post.image} w={16} h={16} mr={5} />
-                <Stack mr={'auto'}>
-                  <Heading fontSize={20} className='text-[#33658a]'>@{post.user_id}
-                   <Text fontSize={12} className='text-gray-700'>{post.created_at.slice(0,16)}</Text>
-                  </Heading>
-                  <Text>{post.title}</Text>
-                 
-                </Stack>
-              </Flex>
-           </Link>
-              
-            <PostMenu state={{post}}/>
-            </CardHeader>
-            <CardBody className='w-[80%] font-raleway border border-gray-400'>
-              <Text>{post.description.slice(0,30)}........</Text>
-              {post.type ? (
-                post.type.includes('image/') ? (
-                  <Image src={post.type} w={'70%'} h={'300px'} />
-                ) : post.type.includes('video/') ? (
-                  <video controls src={post.type} style={{ width: '80%', height: '400px' }} />
-                ) : post.type.includes('audio/') ? (
-                  <audio controls src={post.type} style={{ width: '100%' }} />
-                ) : null
-              ) : (
-                <Text>No media available</Text>
-              )}
-            </CardBody>
-            <CardFooter>
-              <HStack className='font-raleway max-lg:mx-auto '>
-              <Button variant={'ghost'} color={'#33658a'}><CiHeart /></Button>
-                <Button variant={'ghost'} color={'#33658a'}><BiComment /></Button>
-                <Button variant={'ghost'} color={'#33658a'}><RiShareForwardLine /></Button>
-              </HStack>
-            </CardFooter>
-            <Box display={isOpen[post.id] ? "block" : "none"}>
-          <Comments postId={post.id} />
-        </Box>
-          </Card>
-        ))}
-    </SimpleGrid>
-  );
-};
+        fetchContent();
+    }, []);
+
+    return (
+        <SimpleGrid className='lg:w-[60%] overflow-y-scroll gap-4 mx-5 my-3' id='posts'>
+            {content.map(item => (
+                <Card key={item.title} bg={theme.bg} color={theme}>
+                    <CardHeader>
+                        <Flex justify={'space-between'} alignItems={'center'}>
+                            <Image src={item.image} w={16} h={16} mr={5} />
+                            <Stack mr={'auto'}>
+                                <Heading fontSize={20} className='text-[#33658a]'>@{item.title}</Heading>
+                                <Text>{item.description}</Text>
+                            </Stack>
+                            <Text ml={5} pr={8} fontFamily={'Montserrat'} fontSize={14} className='max-md:hidden'>{item.created_at}</Text>
+                        </Flex>
+                    </CardHeader>
+                    <CardBody className='font-raleway'>
+                        <Text>{item.description}</Text>
+                    </CardBody>
+                    <CardFooter>
+                        <HStack className='font-raleway max-lg:mx-auto '>
+                            <Button variant={'ghost'} color={'#33658a'}><CiHeart /></Button>
+                            <Button variant={'ghost'} color={'#33658a'}><BiComment /></Button>
+                            <Button variant={'ghost'} color={'#33658a'}><RiShareForwardLine /></Button>
+                        </HStack>
+                    </CardFooter>
+                </Card>
+            ))}
+        </SimpleGrid>
+    );
+}
 
 export default PostContainer;
