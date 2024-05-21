dab1159cdd345277cca9a2314d5ccbd7429bbd0e
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-21 21:53:14 +0300

    Updating user sign up

--- server/app.py
+++ server/app.py
@@ -93,10 +93,9 @@ class UserResource(Resource):
         username = data.get('username')
         email = data.get('email')
         password_hash = data.get('password')
-        role = None
 
-        if not all([username, email, password_hash, role]):
-            return jsonify({"error": "Username, email, password, and role are required fields"})
+        if not all([username, email, password_hash]):
+            return jsonify({"error": "Username, email and password are required fields"})
 
         user_exists = User.query.filter_by(email=email).first()
         if user_exists:
